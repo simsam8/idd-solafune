df0bcb6cc020741f47511cfedb5f7209734090f5
simsam8 <simvedaa@gmail.com>
2025-04-18 12:55:05 +0200

    remove unused code

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
-import json
 import os
-import shutil
 from pathlib import Path
-
-from ray.train import Checkpoint
 
 from ensemble import EnsembleModel
 from Models import Model
@@ -64,58 +60,5 @@ def load_models(models_path):
     return names, models
 
 
-def save_checkpoint(checkpoint_path, target_path):
-    target_path = Path(target_path)
-    checkpoint_path = (
-        (Path(checkpoint_path.path)).resolve().relative_to(Path("./").resolve())
-    )
-
-    result_path = checkpoint_path.parent / "result.json"
-    result = {}
-    with open(result_path, "r") as f:
-        for line in f.readlines():
-            run_n = json.loads(line)
-            if run_n["checkpoint_dir_name"] == checkpoint_path.name:
-                result = run_n
-                break
-
-    key_rm = [
-        "timestamp",
-        "should_checkpoint",
-        "done",
-        "training_iteration",
-        "time_this_iter_s",
-        "pid",
-        "hostname",
-        "node_ip",
-        "time_since_restore",
-        "iterations_since_restore",
-    ]
-
-    for k in key_rm:
-        del result[k]
-
-    result["checkpoint_path"] = str(checkpoint_path)
-
-    model_name = f"{result['trial_id']}_valf1_{result['val/f1']*100:.2f}"
-
-    os.mkdir(target_path / model_name)
-
-    shutil.copy2(
-        checkpoint_path / "checkpoint.ckpt",
-        target_path / model_name,
-    )
-
-    with open(
-        target_path / model_name / f"{model_name}.json", "w", encoding="utf-8"
-    ) as f:
-        json.dump(result, f, indent=4)
-
-
 if __name__ == "__main__":
-
-    # Testing purposes
-    checkpoint_path = "./logs/tune_idd_asha/train_idd_tune_7c201_00000_0_batch_size=2,lr=0.0083,num_workers=10,weight_decay=0.0084_2025-02-11_13-55-35/checkpoint_000001/"
-    cp = Checkpoint.from_directory(checkpoint_path)
-
-    save_checkpoint(cp, "./models")
+    pass
